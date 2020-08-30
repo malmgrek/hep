bfa089be59088dfb9f0c6adddbb227bcf89af3f5
Stratos Staboulis <stratos.staboulis@leanheat.com>
2020-08-30 21:46:32 +0300

    Fix spacing

--- js/main.js
+++ js/main.js
@@ -29,11 +29,13 @@ const BA = createCard("#1e90ff", "BA")
 const RD = createCard("#dc143c", "RD")
 const RA = createCard("#dc143c", "RA")
 const totenkopf = createCard("#666", "\u2620", "80px")
+const radioactive = createCard("#666", "\u2622", "80px")
+const fu = createCard("#666", "🖕", "80px")
 
 
 function createCards(length = 4) {
     let cardArray = [BD, BA, RD, RA].concat(
-        Array(length - 4).fill(totenkopf)
+        Array(length - 4).fill(radioactive)
     )
     return {array: cardArray, mutated: false}
 }
@@ -51,6 +53,7 @@ let cards = {array: [], mutated: false}
 function drawCard(cards) {
     // Draw (and remove) a card
     index = Math.floor(Math.random() * cards.array.length)
+    // document.getElementById("container").innerHTML = ""
     document.getElementById("container").appendChild(
         cards.array.splice(index, 1).pop().cloneNode(true)  // Adds same element many times
     )
@@ -86,6 +89,7 @@ function createPanel(d) {
     input.autocomplete = "off"
 
     let start = d.createElement("button")
+    start.id = "start"
     start.appendChild(d.createTextNode("Start"))
     start.onclick = () => {
         cards = createCards(
