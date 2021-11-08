6892d22877b4ac0cb32b68dfd3a8ebd97bdad7da
Stratos Staboulis <stratos.staboulis@leanheat.com>
2021-11-08 20:15:48 +0200

    Refactor main script

--- js/main.js
+++ js/main.js
@@ -3,134 +3,140 @@
  */
 
 
-function createCard(backgroundColor, text, fontSize = "20vw") {
+/**
+ * Create player card div's
+ */
+const createCard = (bgColor, text, fontSize = "20vw") => {
 
-    let card = document.createElement("div")
-    card.className = "card"
-    card.style.backgroundColor = backgroundColor
+  let cardText = document.createElement("span");
+  cardText.className = "CardText";
+  cardText.style.fontSize = fontSize;
+  cardText.appendChild(document.createTextNode(text));
 
-    let cardText = document.createElement("span")
-    cardText.className = "cardText"
-    cardText.style.fontSize = fontSize
-    cardText.appendChild(document.createTextNode(text))
+  let card = document.createElement("div");
+  card.className = "Card";
+  card.style.backgroundColor = bgColor;
+  card.appendChild(cardText);
 
-    card.appendChild(cardText)
-    return card
+  return card
 }
-
-
-const blue_defense = createCard("#1e90ff", "BD")
-const blue_offense = createCard("#1e90ff", "BO")
-const red_defense = createCard("#dc143c", "RD")
-const red_offense = createCard("#dc143c", "RO")
-const totenkopf = createCard("#666", "\u2620", "20vw")
-const radioactive = createCard("#ffd700", "\u2622", "20vw")
-const fu = createCard("#666", "🖕", "20vw")
-
 
 
 /**
- * Create a container object for the card deck
+ * Collection of available cards
  */
-function createCards(length = 4) {
-    let cardArray = [
-        blue_defense, blue_offense, red_defense, red_offense
-    ].concat(
-        Array(length - 4).fill(fu)
-    )
-    return {array: cardArray, mutated: false}
+const blue_defense = createCard("#1e90ff", "BD");
+const blue_offense = createCard("#1e90ff", "BO");
+const red_defense = createCard("#dc143c", "RD");
+const red_offense = createCard("#dc143c", "RO");
+// const totenkopf = createCard("#666", "\u2620", "20vw");
+// const radioactive = createCard("#ffd700", "\u2622", "20vw");
+const fu = createCard("#666", "🖕", "20vw");
+
+
+/**
+ * Create a card deck
+ */
+const createCardDeck = (length = 4) => {
+  let cards = [blue_defense, blue_offense, red_defense, red_offense].concat(
+    Array(length - 4).fill(fu));
+  return {cards: cards, mutated: false}
 }
-
-
-// ====================================
-// Create a mutable container of cards
-// drawn so far.
-let cards = {array: [], mutated: false}
-// ====================================
 
 
 /**
  * Take a card from the card container and add to "container"
  */
-function drawCard(cards) {
-    let index = Math.floor(Math.random() * cards.array.length)
-    document.getElementById("container").appendChild(
-        // Adds same element (possibly) many times
-        cards.array.splice(index, 1).pop().cloneNode(true)
-    )
-    cards.mutated = true
+const sampleCard = (cardDeck, parentElement) => {
+
+  let index = Math.floor(Math.random() * cardDeck.cards.length);
+
+  // Remove a random card from deck
+  let card = cardDeck.cards.splice(index, 1).pop().cloneNode(true);
+
+  // Adds same element (possibly) many times
+  parentElement.appendChild(card);
+
+  return {cards: cardDeck.cards, mutated: true}
+
+}
+
+
+const createTextDiv = (text) => {
+
+  let p = document.createElement("p");
+  p.appendChild(document.createTextNode(text));
+  p.style.fontSize = "5vh";
+  p.style.fontFamily = "Courier";
+
+  let div = document.createElement("div");
+  div.appendChild(p);
+
+  return div
+}
+
+
+const App = () => {
+
+  /*************************
+   * Create main container *
+   *************************/
+  let samplingArea = document.createElement("div");
+  samplingArea.id = "samplingArea";
+  samplingArea.appendChild(createTextDiv(
+    "Select the number of players and click Hep!"));
+
+  /************************
+   * Create control panel *
+   ************************/
+  let controlPanel = document.createElement("div");
+  controlPanel.className = "ControlPanel";
+
+  // Input field that defines number of players
+  let playersInput = document.createElement("input");
+  playersInput.type = "number";
+  playersInput.id = "playersInput";
+  playersInput.min = 4;
+  playersInput.value = 4;
+  playersInput.autocomplete = "off";
+
+  // Hep! button that initializes the card sampling loop
+  let startButton = document.createElement("button");
+  startButton.id = "startButton";
+  startButton.appendChild(document.createTextNode("Hep!"));
+  // Button triggers re-creation of a new card deck
+  // according player number given by the user
+  startButton.addEventListener("click", () => {
+    cardDeck = createCardDeck(
+      document.getElementById("playersInput").value);
+    samplingArea = document.getElementById("samplingArea");
+    samplingArea.innerHTML = "";
+    samplingArea.appendChild(createTextDiv(
+      "Tap screen to draw cards!"));
+    // Add click (tap) detection to all over main container.
+    // Triggers new random cards appearing on screen
+    samplingArea.addEventListener("click", () => {
+        // TODO: Get rid of 'mutated'
+        cardDeck = (cardDeck.mutated && cardDeck.cards.length) ?
+        sampleCard(cardDeck, samplingArea) :
+        {cards: cardDeck.cards, mutated: true}
+    });
+  });
+
+  /****************************************
+   * Render - Append elements to document *
+   ****************************************/
+  controlPanel.appendChild(playersInput);
+  controlPanel.appendChild(startButton);
+  samplingArea.appendChild(controlPanel);
+  document.body.appendChild(samplingArea);
+
+  return true
+
 }
 
 
 /**
- * Create a container where we can draw new cards by tapping
+ * Run app
  */
-function createContainer() {
-
-    const mutate = () => {
-        cards.mutated = true
-    }
-
-    let container = document.createElement("div")
-    container.id = "container"
-    container.addEventListener("click", () => {
-        (cards.mutated && cards.array.length) ?
-            drawCard(cards) : mutate()
-    })
-    return container
-}
-
-
-/**
- * Build the main component
- */
-function createPanel() {
-
-    let panel = document.createElement("div")
-    panel.className = "panel"
-
-    let input = document.createElement("input")
-    input.type = "number"
-    input.id = "number-of-cards"
-    input.min = 4
-    input.value = 4
-    input.autocomplete = "off"
-
-    let start = document.createElement("button")
-    start.id = "start"
-    start.appendChild(document.createTextNode("Hep!"))
-    start.addEventListener("click", () => {
-        cards = createCards(document.getElementById("number-of-cards").value)
-        container = document.getElementById("container")
-        container.innerHTML = ""
-        container.appendChild(createInfo(document, "Tap screen to draw cards!"))
-    })
-
-    panel.appendChild(input)
-    panel.appendChild(start)
-
-    return panel
-}
-
-
-function createInfo(text) {
-    let div = document.createElement("div")
-    let p = document.createElement("p")
-    p.appendChild(document.createTextNode(text))
-    p.style.fontSize = "5vh"
-    p.style.fontFamily = "Courier"
-    div.appendChild(p)
-    return div
-}
-
-
-//
-// Build document body
-//
-
-container = createContainer()
-container.appendChild(createInfo(
-    "Select the number of players and click Hep!"
-))
-container.appendChild(createPanel())
-document.body.appendChild(container)
+let status = App();
